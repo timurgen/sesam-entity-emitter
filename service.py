1ddc1d441c8d289185672ebe07ab237e05925359
Timur Samkharadze <timur.samkharadze@akersolutions.com>
2020-08-11 18:05:22 +0200

    updated to Python 3.8

--- service.py
+++ service.py
@@ -13,8 +13,8 @@ from flask import Flask, request, Response
 
 app = Flask(__name__)
 
-adj_list = []
-noun_list = []
+a_lst = []
+n_lst = []
 
 
 def str_to_bool(s: str): return s.lower() == 'true'
@@ -34,23 +34,24 @@ def stream_json_list(generator):
 @app.route('/', methods=['GET'])
 def generate():
     n = int(request.args.get('n', 100))
-    n_fields = int(request.args.get('n_fields', 1))
+    n_f = int(request.args.get('n_fields', 1))
     with_updated = str_to_bool(request.args.get('with_updated', 'False'))
     return Response(
         stream_json_list(
-            ({**{'_id': str(x), '_updated': time.time() if with_updated else None},
-              **{f'{choice(adj_list)}_{choice(noun_list)}': f'{choice(adj_list)}_{choice(noun_list)}' for _ in
-                 range(0, n_fields)}}
-
-             for x in range(1, n + 1))
+            (
+                {
+                    **{'_id': str(x), '_updated': time.time() if with_updated else None},
+                    **{f'{choice(a_lst)}_{choice(n_lst)}': f'{choice(a_lst)}_{choice(n_lst)}' for _ in range(0, n_f)}
+                } for x in range(1, n + 1)
+            )
         ), mimetype='application/json'
     )
 
 
 if __name__ == '__main__':
     with open('english-adjectives.txt', 'r') as f1:
-        adj_list = f1.read().splitlines()
+        a_lst = f1.read().splitlines()
 
     with open('english-nouns.txt', 'r') as f2:
-        noun_list = f2.read().splitlines()
+        n_lst = f2.read().splitlines()
     app.run('0.0.0.0', debug=True, port=int(os.environ.get('PORT', 5000)))
